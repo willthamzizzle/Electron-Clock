fc24633f92fde90cd4b0c89082df5dbef538cfdc
William Thammavong <thammavo@eng.utah.edu>
2015-11-05 16:42:06 -0700

    doing stuff

--- main.js
+++ main.js
@@ -1,50 +1,75 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var globalShortcut = require('global-shortcut');
+//var configuration = require('./configuration');
 var ipc = require('ipc');
 
-// Report crashes to our server.
-require('crash-reporter').start();
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
+var settingsWindow = null;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    frame: false,
-    resizable: false,
-    width: 800,
-    height: 600
-  });
+    //if (!configuration.readSettings('shortcutKeys')) {
+    //    configuration.saveSettings('shortcutKeys', ['ctrl', 'shift']);
+    //}
 
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/MyApp/index.html');
+    mainWindow = new BrowserWindow({
+        frame: false,
+        height: 300,
+        resizable: true,
+        width: 200
+    });
 
-  // Open the DevTools.
-  // mainWindow.openDevTools();
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    //setGlobalShortcuts();
 });
 
+/*function setGlobalShortcuts() {
+    globalShortcut.unregisterAll();
+
+    var shortcutKeysSetting = configuration.readSettings('shortcutKeys');
+    var shortcutPrefix = shortcutKeysSetting.length === 0 ? '' : shortcutKeysSetting.join('+') + '+';
+
+    globalShortcut.register(shortcutPrefix + '1', function () {
+        mainWindow.webContents.send('global-shortcut', 0);
+    });
+    globalShortcut.register(shortcutPrefix + '2', function () {
+        mainWindow.webContents.send('global-shortcut', 1);
+    });
+}*/
 
 ipc.on('close-main-window', function () {
     app.quit();
 });
+/*
+ipc.on('open-settings-window', function () {
+    if (settingsWindow) {
+        return;
+    }
+
+    settingsWindow = new BrowserWindow({
+        frame: false,
+        height: 300,
+        resizable: true,
+        width: 200
+    });
+
+    settingsWindow.loadUrl('file://' + __dirname + '/app/settings.html');
+
+    settingsWindow.on('closed', function () {
+        settingsWindow = null;
+    });
+});
+
+ipc.on('close-settings-window', function () {
+    if (settingsWindow) {
+        settingsWindow.close();
+    }
+});
+
+ipc.on('set-global-shortcuts', function () {
+    setGlobalShortcuts();
+});
+*/
